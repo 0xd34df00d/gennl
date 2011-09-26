d107f6c20123c1d75e685a42e6c49ca7a7cc7fd5
0xd34df00d <0xd34df00d@gmail.com>
2011-09-26 19:25:47 +0400

    Forgot FormatClass.

---
+++ hs/FormatClass.hs
@@ -0,0 +1,12 @@
+module FormatClass
+    where
+
+class Show a => Formattable a where
+    pretty :: a -> String
+    pretty = show
+
+instance Formattable Int
+instance Formattable Double
+
+instance (Formattable a, Formattable b) => Formattable (a, b) where
+    pretty (a, b) = "(" ++ pretty a ++ ", " ++ pretty b ++ ")"
