4b9a0da3b398d87a06a330a2eeb56cdd4b142e05
0xd34df00d <0xd34df00d@gmail.com>
2011-11-21 06:15:05 +0400

    Allow to take some but not all points from file.

--- hs/Main.hs
+++ hs/Main.hs
@@ -27,23 +27,23 @@ parsed s = applyP' id s
 simplified s = applyP' simplifyStab s
 --incMatrix s = applyP' toIncMatrix s
 
-runStuff (Left m) _ _ = error $ show m
-runStuff (Right recs) num g = runStuff' (map (map read) recs) num g
+runStuff (Left m) _ _ _ = error $ show m
+runStuff (Right recs) num pts g = runStuff' (map (map read) (take (pts * pts) recs)) num g
 
 runStuff' recs num g = (pretty a, fit, iter st, map snd $ fits st)
     where defGA = initPpl num $ initGA (cfg { vars = ["x", "y"], testSet = map (take 2 &&& head . drop 2) recs, optNum = num } ) g
           cfg = defConfig :: GAConfig (ExprTree (Double))
           (a, fit, st) = runGA defGA
 
-genSynth f ni nj = intercalate "\r\n" $ map (intercalate "," . map show) [ [i, j, f i j ] | i <- [1..ni], j <- [1..nj] ]
+genSynth f ni nj = intercalate "\r\n" $ map (intercalate "," . map show) [ [i, j, f i j ] | i <- map (+3) [1..ni], j <- map (+3) [1..nj] ]
 
-main' :: Int -> Double -> IO ()
+main' :: Int -> Int -> IO ()
 main' num pts = do
-    let recs = parseCSV (genSynth (\x y -> (exp 1) ** ((x / y + y / x) / 10) + 1) pts pts ++ "\r\n")
-    --file <- readFile "options.dat.txt"
-    --let recs = parseCSV file
+    --let recs = parseCSV (genSynth (\x y -> (exp 1) ** ((x / y + y / x) / 10) + 1) pts pts ++ "\r\n")
+    file <- readFile "options.dat.txt"
+    let recs = parseCSV file
     g <- newStdGen
-    print $ runStuff recs num g
+    print $ runStuff recs num pts g
 
 main = do
     args <- getArgs
