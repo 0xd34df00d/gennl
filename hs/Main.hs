79dd09e9d8fb54a8c52b1f71599a674f8eed98c4
0xd34df00d <0xd34df00d@gmail.com>
2012-10-10 02:49:12 +0400

    Main initialization changes.

--- hs/Main.hs
+++ hs/Main.hs
@@ -29,18 +29,18 @@ runStuff (Right recs) num pts g = runStuff' (map (map read) (take (pts * pts) re
     where recs' = map (\(x:y:z:[]) -> y:z:x:[]) recs
 
 runStuff' recs num g = (pretty a, fit, iter st, map snd $ fits st)
-    where defGA = initPpl num $ initGA (cfg { vars = ["x", "y"], testSet = map (take 2 &&& head . drop 2) recs, optNum = num } ) g
-          cfg = defConfig :: GAConfig (ExprTree (Double))
-          (a, fit, st) = runGA defGA
+    where cfg = defConfig :: GAConfig (ExprTree (Double))
+          defGA = initPpl num $ initGA (cfg { vars = ["x", "y"], testSet = map (take 2 &&& head . drop 2) recs, optNum = num } ) g
+          (a, fit, st) = (map pretty (ppl defGA)) `traceShow` runGA defGA
 
 genSynth f ni nj = intercalate "\r\n" $ map (intercalate "," . map show) [ [i, j, f i j ] | i <- map (+3) [1..ni], j <- map (+3) [1..nj] ]
 
 main' :: Int -> Double -> IO ()
 main' num pts = do
     --let recs = parseCSV (genSynth (\x y -> (exp 1) ** ((x / y + y / x) / 10) + 1) pts pts ++ "\r\n")
-    --let recs = parseCSV (genSynth (\x y -> x * cos (y)) pts pts ++ "\r\n")
-    file <- readFile "options.dat.txt"
-    let recs = parseCSV file
+    let recs = parseCSV (genSynth (\x y -> x * cos (y)) pts pts ++ "\r\n")
+    --file <- readFile "options.dat.txt"
+    --let recs = parseCSV file
     g <- newStdGen
     print $ runStuff recs num (floor pts) g
 
