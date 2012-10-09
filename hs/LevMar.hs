9e3f36b761368604c754129556ef61bd496cb897
0xd34df00d <0xd34df00d@gmail.com>
2012-10-09 21:18:02 +0400

    Freaking working LevMar.

--- hs/LevMar.hs
+++ hs/LevMar.hs
@@ -7,19 +7,19 @@ import Control.Arrow
 import qualified Control.Monad as CM (join)
 import Data.List
 import Foreign.Storable
-import Debug.Trace
 import Data.Packed.Matrix
 import Data.Packed.Vector
 import Numeric.LinearAlgebra
-
 import SupportUtils
 
 class (Product a, Num (Vector a), Field a, Container Vector a, Element a, Storable a, RealFloat a) => GAMX a
+instance GAMX Double
 
 -- delta = (JtJ + lambda diag (JtJ))^-1 Jt (y - f(beta))
 
 -- Model: function taking a list of params and a list of independent variables.
 type Model a = ([a], [a]) -> a
+-- Jacobian: function taking a list of params and a list of independent variables and returning list of partial derivatives.
 type Jacob a = ([a], [a]) -> [a]
 
 type MModel a = (Vector a, Vector a) -> a
@@ -53,66 +53,23 @@ fitModel' iter λ sse f j (ys, xs) β | iter > 20 || shStop = β
           v = 5.5
           (λd, λu) = (λ / v, λ * v)
           ssed = modelSSE f (ys, xs) (β + δd)
-          δd = δfor λd
-          δfor λ = inv (js + mapMatrix (* λ) (diag $ takeDiag js)) <> jmt <> (ys - vecFun f (β, xs))
+          δd = inv (js + mapMatrix (* λd) (diag $ takeDiag js)) <> jmt <> (ys - vecFun f (β, xs))
           (iter', sse', λ', β') | ssed <= sse = (iter + 1, ssed, λd, β + δd)
                                 | otherwise = (iter, sse, λu, β)
 
-{-
-type Model a = ([a], [a]) -> a
-type Jacob a = ([a], [a]) -> [a]
-
-type MModel a = (Matrix a, Matrix a) -> a
-type MJacob a = (Matrix a, Matrix a) -> Matrix a
-
-vecFun :: Num a => MModel a -> Matrix a -> Matrix a -> Matrix a
-vecFun f β xs = fromCols [map (\x -> f (β, x)) xs']
-    where xs' = rowsAsMats xs
-
-jacMat :: Num a => MJacob a -> Matrix a -> Matrix a -> Matrix a
-jacMat j β xs = fromRows $ map (\x -> concat $ rows $ j (β, x)) xs'
-    where xs' = rowsAsMats xs
-
-f2v :: Model a -> MModel a
-f2v f p = f (join (***) (concat . cols) p)
-
-j2v :: Jacob a -> MJacob a
-j2v j p = fromRows [j (join (***) (concat . cols) p)]
-
-modelSSE :: (Floating a) => MModel a -> (Matrix a, Matrix a) -> Matrix a -> a
-modelSSE f (ys, xs) β = sqrt (absMVec (ys -|- vecFun f β xs))
-
-fitModel :: (Ord a, RealFloat a) => Model a -> Jacob a -> [(a, [a])] -> [a] -> [a]
-fitModel f j pts β = concat $ cols $ fitModel' 0 0.01 sse (f2v f) (j2v j) (yv, xv) βv
-    where yv = fromCols [map fst pts]
-          xv = fromRows $ map snd pts
-          βv = fromCols [β]
+fitModel :: (GAMX a) => Model a -> Jacob a -> [(a, [a])] -> [a] -> Vector a
+fitModel f j pts b = fitModel' 0 0.01 sse (f2v f) (j2v j) (yv, xv) βv
+    where yv = fromList $ map fst pts
+          xv = fromRows $ map (fromList . snd) pts
+          βv = fromList b
           sse = modelSSE (f2v f) (yv, xv) βv
-
-fitModel' :: (Ord a, RealFloat a) => Int -> a -> a -> MModel a -> MJacob a -> (Matrix a, Matrix a) -> Matrix a -> Matrix a
-fitModel' iter λ sse f j (ys, xs) β | iter > 20 || shStop = β
-                                    | otherwise = {-(sse, ssed, iter', λ', β') `traceShow`-} fitModel' iter' λ' sse' f j (ys, xs) β'
-    where δfor λ = invMatLU (js +|+ λ *| diag js) *|* jmt *|* (ys -|- vecFun f β xs)
-          jm = jacMat j β xs
-          jmt = trp jm
-          js = jmt *|* jm
-          shStop | iter' > iter = absMVec (β' -|- β) < min 0.00001 (absMVec β / 50) || isInfinite sse || isInfinite ssed
-                 | otherwise = λ > 1e9 || isInfinite sse || isInfinite ssed
-          ν = 5.5
-          (λd, λu) = (λ / ν, λ * ν)
-          δd = δfor λd
-          ssed = modelSSE f (ys, xs) (β +|+ δd)
-          (iter', sse', λ', β') | ssed <= sse = (iter + 1, ssed, λd, β +|+ δd)
-                                | otherwise = (iter, sse, λu, β)
---{-# SPECIALIZE fitModel' :: Int -> Double -> Double -> MModel Double -> MJacob Double -> (Matrix Double, Matrix Double) -> Matrix Double -> Matrix Double #-}
 
 f1 :: Floating a => Model a
 f1 (a:b:c:[], x:y:[]) = a * x + b * y + c * y * sin x
 
 j1 :: (Real a, Fractional a, Floating a) => Jacob a
-j1 (cs@(a:b:c:[]), xs@(x:y:[])) = concat $ jacobian (\cts -> [f1 (cts, map realToFrac xs)]) cs
+j1 ((a:b:c:[]), (x:y:[])) = [x, y, y * sin x]--[a + c * y * cos x, b + c * sin x]
 
 xs = [[x, y] | x <- [0.0 .. 5.0], y <- [0.0 .. 5.0]]
 ys = map (\x -> f1 ([4, 5, 2], x)) xs
 pts = zip ys xs
--}
